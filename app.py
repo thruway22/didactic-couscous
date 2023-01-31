2b5f2f73121bc747644c5e95b1df5e0786f7ecbd
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-01-31 23:51:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('NextTrade')
+uploaded_file = st.file_uploader("Choose a file", type='CSV')
+dataframe = pd.read_csv(uploaded_file)
+st.write(dataframe)
