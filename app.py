7224801cd2f4e3ba2dc679429e4b94b853c3dd2c
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-01 00:01:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('NextTrade')
 uploaded_file = st.file_uploader("Choose a file", type='CSV')
 
 
-if uploaded_file is not None:
+if uploaded_file is None:
   pass
 else:
   dataframe = pd.read_csv(uploaded_file)
